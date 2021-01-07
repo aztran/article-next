2fb21224e79713fe374e6fb327e66637a85762c9
aztran <andra.riztyan@gmail.com>
2021-01-07 22:51:25 +0700

    remove nprogress & toastify

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,20 @@
 import Router from 'next/router';
-import { ToastContainer } from 'react-toastify';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
+// import { ToastContainer } from 'react-toastify';
+// import NProgress from 'nprogress';
+// import 'nprogress/nprogress.css';
 import '../../tailwindcss/style.css';
-import 'react-toastify/dist/ReactToastify.css';
+// import 'react-toastify/dist/ReactToastify.css';
 
-NProgress.configure({ showSpinner: false }); //showSpinner: false
+// NProgress.configure({ showSpinner: false }); //showSpinner: false
 
-Router.events.on('routeChangeStart', () => NProgress.start());
-Router.events.on('routeChangeComplete', () => NProgress.done());
-Router.events.on('routeChangeError', () => NProgress.done());
+// Router.events.on('routeChangeStart', () => NProgress.start());
+// Router.events.on('routeChangeComplete', () => NProgress.done());
+// Router.events.on('routeChangeError', () => NProgress.done());
 function MyApp({ Component, pageProps }) {
 	return (
 		<>
 			<Component {...pageProps} />
-			<ToastContainer position='top-center'> </ToastContainer>
+			{/* <ToastContainer position='top-center'> </ToastContainer> */}
 		</>
 	);
 }
